61b1627361073a48fbfb81f348ec729c31372e0f
emmaflames125-alt <emmaflames125@gmail.com>
2026-04-05 18:31:34 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/investment-pwa-kr90/',   // ← CHANGED TO YOUR REPO NAME
+  base: '/investment-pwa-kr90-/',   // ← CHANGED TO YOUR REPO NAME
   plugins: [
     react(),
     VitePWA({
